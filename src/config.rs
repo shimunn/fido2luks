42945956a6cd406e8607b8897e386ea18fbebdf8
Jan Niehusmann <jan@gondor.com>
2019-10-11 22:04:00 +0000

    Add test case for hash calculations
    While replacing the implementation of sha256, I noticed that there
    is no test case actually calling the hash calculations.
    
    Added two such test cases. Please note that I didn't verify that the
    result is correct, but just took the value the existing implementation
    returned. So those tests will only catch future regressions.

--- src/config.rs
+++ src/config.rs
@@ -170,4 +170,12 @@ mod test {
         assert_eq!("ask".parse::<InputSalt>().unwrap(), InputSalt::AskPassword);
         assert_eq!("lol".parse::<InputSalt>().unwrap(), InputSalt::default());
     }
+
+    #[test]
+    fn input_salt_obtain() {
+        assert_eq!(
+            InputSalt::String("abc".into()).obtain(&PasswordHelper::Stdin).unwrap(),
+            [186, 120, 22, 191, 143, 1, 207, 234, 65, 65, 64, 222, 93, 174, 34, 35, 176, 3, 97, 163, 150, 23, 122, 156, 180, 16, 255, 97, 242, 0, 21, 173]
+        )
+    }
 }

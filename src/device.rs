e7049a281a63e3f72f83b720eb44ccb0c65ed4e1
Shimun <shimun@shimun.net>
2020-01-02 15:31:55 +0100

    Use fido2luks as rp_id instead if default hmac, consider making credenials device specific

--- src/device.rs
+++ src/device.rs
@@ -1,8 +1,10 @@
 use crate::error::*;
 
-use ctap;
-use ctap::extensions::hmac::{FidoHmacCredential, HmacExtension};
-use ctap::{FidoDevice, FidoError, FidoErrorKind};
+use ctap::{
+    self,
+    extensions::hmac::{FidoHmacCredential, HmacExtension},
+    FidoDevice, FidoError, FidoErrorKind,
+};
 
 pub fn make_credential_id() -> Fido2LuksResult<FidoHmacCredential> {
     let mut errs = Vec::new();
@@ -10,7 +12,11 @@ pub fn make_credential_id() -> Fido2LuksResult<FidoHmacCredential> {
         ref devs if devs.is_empty() => Err(Fido2LuksError::NoAuthenticatorError)?,
         devs => {
             for mut dev in devs.into_iter() {
-                match dev.make_hmac_credential() {
+                match dev
+                    .make_credential("fido2luks", &[0u8], "", &[0u8; 32])
+                    .map(|cred| cred.into())
+                {
+                    //TODO: make credentials device specific
                     Ok(cred) => {
                         return Ok(cred);
                     }

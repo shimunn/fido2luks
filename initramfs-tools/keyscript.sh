0245c88c4a2f22746b3cd3b5ff26f299a25f6352
shimun <shimun@shimun.net>
2020-08-11 22:38:58 +0200

    get plymouth to display a message

--- initramfs-tools/keyscript.sh
+++ initramfs-tools/keyscript.sh
@@ -3,7 +3,9 @@ set -a
 . /etc/fido2luks.conf
 
 if [ -z "$FIDO2LUKS_PASSWORD_HELPER" ]; then
-	export FIDO2LUKS_PASSWORD_HELPER="plymouth ask-for-password --promt 'FIDO2 password salt for $CRYPTTAB_NAME'"
+	MSG="FIDO2 password salt for $CRYPTTAB_NAME"
+	export FIDO2LUKS_PASSWORD_HELPER="plymouth ask-for-password --promt '$MSG'"
+	plymouth message --text "$MSG"
 fi
 
 fido2luks print-secret --bin

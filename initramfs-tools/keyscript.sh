8465949b44a0004f1020b61359226bcb33e5596a
shimun <shimun@shimun.net>
2020-08-16 13:42:35 +0200

    spell promPt correctly

--- initramfs-tools/keyscript.sh
+++ initramfs-tools/keyscript.sh
@@ -4,8 +4,7 @@ set -a
 
 if [ -z "$FIDO2LUKS_PASSWORD_HELPER" ]; then
 	MSG="FIDO2 password salt for $CRYPTTAB_NAME"
-	export FIDO2LUKS_PASSWORD_HELPER="plymouth ask-for-password --promt '$MSG'"
-	plymouth message --text "$MSG"
+	export FIDO2LUKS_PASSWORD_HELPER="plymouth ask-for-password --prompt '$MSG'"
 fi
 
 fido2luks print-secret --bin

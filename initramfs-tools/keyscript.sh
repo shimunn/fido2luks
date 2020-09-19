55bae4161e1e1c5a7fc37fc0e565bff398da94b3
shimun <shimun@shimun.net>
2020-09-19 18:23:21 +0200

    add credentials situated in the luks header to credential list

--- initramfs-tools/keyscript.sh
+++ initramfs-tools/keyscript.sh
@@ -7,4 +7,8 @@ if [ -z "$FIDO2LUKS_PASSWORD_HELPER" ]; then
 	export FIDO2LUKS_PASSWORD_HELPER="plymouth ask-for-password --prompt '$MSG'"
 fi
 
+if [ "$FIDO2LUKS_USE_TOKEN" -eq 1 ]; then
+	export FIDO2LUKS_CREDENTIAL_ID="$FIDO2LUKS_CREDENTIAL_ID,$(fido2luks token list --csv $CRYPTTAB_SOURCE)"
+fi
+
 fido2luks print-secret --bin

871b2863b2dbd49d4448cc09302ef901099471dd
shimun <shimun@shimun.net>
2022-09-28 19:48:30 +0200

    fix: build env

--- build.rs
+++ build.rs
@@ -11,17 +11,22 @@ mod util;
 
 use cli_args::Args;
 use std::env;
+use std::fs;
+use std::path::PathBuf;
 use std::str::FromStr;
 use structopt::clap::Shell;
 use structopt::StructOpt;
 
 fn main() {
+    let env_outdir = env::var_os("OUT_DIR").unwrap();
+    let outdir = PathBuf::from(PathBuf::from(env_outdir).ancestors().nth(3).unwrap());
+    fs::create_dir_all(&outdir).unwrap();
     // generate completion scripts, zsh does panic for some reason
     for shell in Shell::variants().iter().filter(|shell| **shell != "zsh") {
         Args::clap().gen_completions(
             env!("CARGO_PKG_NAME"),
             Shell::from_str(shell).unwrap(),
-            env!("CARGO_MANIFEST_DIR"),
+            &outdir,
         );
     }
 }
--- flake.nix
+++ flake.nix
@@ -9,32 +9,42 @@
     };
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
+outputs = inputs @ { self, nixpkgs, utils, naersk, ... }:
     let
-      root = ./.;
-      pname = (builtins.fromTOML (builtins.readFile ./Cargo.toml)).package.name;
-      forPkgs = pkgs:
-        let
-          naersk-lib = naersk.lib."${pkgs.system}";
-          buildInputs = with pkgs; [ cryptsetup libusb.dev ];
-          LIBCLANG_PATH = "${pkgs.clang.cc.lib}/lib";
-          nativeBuildInputs = with pkgs; [
-            pkgconfig
-            clang
-          ];
-        in
+      root = inputs.source or self;
+      pname = (builtins.fromTOML (builtins.readFile (root + "/Cargo.toml"))).package.name;
+      # toolchains: stable, beta, default(nightly)
+      toolchain = pkgs: if inputs ? fenix then inputs.fenix.packages."${pkgs.system}".complete.toolchain 
+                  else with pkgs; symlinkJoin { name = "rust-toolchain"; paths = [ rustc cargo ]; };
+      forSystem = system:
+      let 
+        pkgs = nixpkgs.legacyPackages."${system}";
+      in
         rec {
           # `nix build`
-          packages.${pname} = naersk-lib.buildPackage {
-            inherit pname root buildInputs nativeBuildInputs LIBCLANG_PATH;
-          };
-          defaultPackage = packages.${pname};
+          packages.${pname} = (self.overlay pkgs pkgs).${pname};
+
+          packages.dockerImage = pkgs.runCommandLocal "docker-${pname}.tar.gz" {} "${apps.streamDockerImage.program} | gzip --fast > $out";
+
+          packages.default = packages.${pname};
 
           # `nix run`
           apps.${pname} = utils.lib.mkApp {
             drv = packages.${pname};
           };
-          defaultApp = apps.${pname};
+          
+          # `nix run .#streamDockerImage | docker load`
+          apps.streamDockerImage = utils.lib.mkApp {
+            drv = with pkgs; dockerTools.streamLayeredImage {
+              name = pname;
+              tag = self.shortRev or "latest";
+              config = {
+                Entrypoint = apps.default.program;
+              };
+            };
+            exePath = "";
+          };
+          apps.default = apps.${pname};
 
           # `nix flake check`
           checks = {
@@ -49,15 +59,42 @@
           hydraJobs = checks // packages;
 
           # `nix develop`
-          devShell = pkgs.mkShell {
-            nativeBuildInputs = with pkgs; [ rustc cargo rustfmt nixpkgs-fmt ] ++ nativeBuildInputs;
-            inherit buildInputs LIBCLANG_PATH;
+          devShell = pkgs.mkShell rec {
+            RUST_SRC_PATH = "${if inputs ? fenix then "${toolchain pkgs}/lib/rustlib" else pkgs.rustPlatform.rustLibSrc}";
+            nativeBuildInputs = with pkgs; [ (toolchain pkgs) cargo-edit rustfmt nixpkgs-fmt ] ++ packages.default.nativeBuildInputs;
+            inherit (packages.default) buildInputs LIBCLANG_PATH;
+            shellHook = ''
+              printf "Rust version:"
+              rustc --version
+              printf "\nbuild inputs: ${pkgs.lib.concatStringsSep ", " (map (bi: bi.name) (buildInputs ++ nativeBuildInputs))}"
+            '';
           };
-        };
-      forSystem = system: forPkgs nixpkgs.legacyPackages."${system}";
-    in
-    (utils.lib.eachSystem [ "aarch64-linux" "i686-linux" "x86_64-linux" ] forSystem) // {
-      overlay = final: prev: (forPkgs final).packages;
-    };
 
+        };
+    in
+    (utils.lib.eachDefaultSystem forSystem) // {
+      overlays.pinned = final: prev: (self.overlay final (import nixpkgs {
+        inherit (final) localSystem;
+      })).packages;
+      overlay = final: prev:
+        let
+          naersk-lib = naersk.lib."${final.system}".override {
+            rustc = toolchain prev;
+            cargo = toolchain prev;
+          };
+          buildInputs = with prev; [ 
+            udev cryptsetup.dev
+          ];
+          nativeBuildInputs = with prev; [
+            pkg-config clang
+          ];
+        in
+        {
+          "${pname}" =
+            naersk-lib.buildPackage {
+              LIBCLANG_PATH = "${final.llvmPackages.libclang.lib}/lib";
+              inherit pname root buildInputs nativeBuildInputs;
+            };
+        };
+    };
 }
